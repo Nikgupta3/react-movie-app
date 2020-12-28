c93e37b2371a15d61cc504b119f5f128e1fbe4ba
Nikgupta3 <nik_gupta3@live.com>
2020-12-28 08:46:36 +0530

    fix img tag 1

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ const Result = (props) => {
     console.log(props.result)
   return (
     <div className="result">
-        <img src={props.result.Poster} alt=""/>
+        <img src={props.result.Poster} alt="No preview available"/>
         <h3>{props.result.Title} ({props.result.Year})</h3>
     </div>
   );
