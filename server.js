111a4ce5c6f4b9dbe8845fed92fe43122dbd27c4
Nikgupta3 <nik_gupta3@live.com>
2021-01-02 16:03:06 +0530

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 1337;
 
+app.use(cors());
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'build')));
 // Handle React routing, return all requests to React app
